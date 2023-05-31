a23d2939664640e5e4935cb52be5f9f1f0943f53
QizhangLi <liqizhang95@gmail.com>
2023-05-31 14:15:12 +0800

    Update morebayesian.py

--- attacks/morebayesian.py
+++ attacks/morebayesian.py
@@ -7,18 +7,18 @@ from .helper import random_start_function, to_np_uint8, update_and_clip
 from torchvision import transforms as T
 from PIL import Image
 
-
 def get_input_grad(x, y, model_list):
     ce_grad_sum = 0
     loss_sum = 0
-    model = random.choice(model_list)
-    x.requires_grad_(True)
-    out = model(x)
-    loss = F.cross_entropy(out, y)
-    ce_grad = torch.autograd.grad(loss, [x, ])[0]
-    final_grad = ce_grad.data
-    loss_sum = loss.item()
-    return final_grad, loss_sum
+    for model in model_list:
+        x.requires_grad_(True)
+        out = model(x)
+        loss = F.cross_entropy(out, y)
+        ce_grad_sum += torch.autograd.grad(loss, [x, ])[0].data
+        loss_sum += loss.item()
+    final_grad = ce_grad_sum / len(model_list)
+    loss_avg = loss_sum / len(model_list)
+    return final_grad, loss_avg
 
 
 def morebayesian_attack(args, ori_img, label, mean_model, sqmean_model, get_model_list, verbose=True):
